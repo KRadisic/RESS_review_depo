52626ebbe09208633b75fb4f786f09dc62f9610a
KRadisic <katarina.radisic96@gmail.com>
2026-03-09 16:28:24 +0100

    plot raw input data

--- visualize_raw_input_data_RESS.R
+++ visualize_raw_input_data_RESS.R
@@ -12,63 +12,33 @@ data_directory <- "~/Documents/RESS_review_depo/"
 
 setwd(data_directory)
 
+## Load input PESHMELBA data
+Nx_train <- 50
+Nx_test <- 100
 LHS_train <- as.matrix(npyLoad("LHS_rep5_50_R500_dim6_sample_final.npy"))
 LHS_test <- as.matrix(npyLoad("LHS25_100_R500_dim6_sample_final.npy"))
-LHS_enriched_test <- as.matrix(npyLoad("LHS110_200_R300_dim6_sample_final.npy"))
+#LHS_enriched_test <- as.matrix(npyLoad("LHS110_200_R300_dim6_sample_final.npy"))
 
+## Indices of parameters used in metamodeling
 indices_6_parms <- c(63, 65, 68, 71, 72, 99)
+names <- read.csv('entry_factor_names.csv', header = FALSE)
+names$V1[indices_6_parms]
 
-plotting_data_train <- data.frame(cbind(LHS_train[1:50,indices_6_parms],rep('train', 50)))
-plotting_data_test <- data.frame(cbind(LHS_test[1:100,indices_6_parms],rep('test', 100)))
-plotting_data_enriched_test <- data.frame(cbind(LHS_enriched_test[1:110,indices_6_parms],rep('enriched_test', 110)))
+## Arrange the data into a dataframe for ggplot plotting
+plotting_data_train <- data.frame(cbind(LHS_train[1:Nx_train,indices_6_parms],rep('train', Nx_train)))
+plotting_data_test <- data.frame(cbind(LHS_test[1:Nx_test,indices_6_parms],rep('test', Nx_test)))
+#plotting_data_enriched_test <- data.frame(cbind(LHS_enriched_test[1:110,indices_6_parms],rep('enriched_test', 110)))
 
-
-df <- data.frame(rbind(plotting_data_train, plotting_data_test, plotting_data_enriched_test))
+df <- data.frame(rbind(plotting_data_train, plotting_data_test))#, plotting_data_enriched_test))
 df <- data.frame(as.numeric(df[,1]),as.numeric(df[,2]),as.numeric(df[,3]),
                       as.numeric(df[,4]),as.numeric(df[,5]),as.numeric(df[,6]),'type' = df[,7])
+colnames(df) <- c(names$V1[indices_6_parms], 'type')
 head(df)
 
 ## Pair plot for raw train and test set.
-ggpairs(df,aes(pch = type,color = type, alpha = 0.5),
+ggpairs(df,aes(pch = type,color = type, alpha = 0.5), columns = c(1:6),
         lower = list(continuous = wrap("points", alpha = 0.5, size=1, pch = 1), 
                      combo = wrap("dot", alpha = 0.4, size=0.2) ),
         upper = list(continuous = wrap("points", alpha = 0.5, size=1, pch = 1), 
                      combo = wrap("dot", alpha = 0.4, size=0.2) )) +theme_bw() +
   theme(legend.position = 'bottom') 
-
-## Param. 1
-df_test <- data.frame(as.numeric(df[,1]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
-## Param. 2
-df_test <- data.frame(as.numeric(df[,2]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
-## Param. 3
-df_test <- data.frame(as.numeric(df[,3]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
-## Param. 4
-df_test <- data.frame(as.numeric(df[,4]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
-## Param. 5
-df_test <- data.frame(as.numeric(df[,5]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
-## Param. 6
-df_test <- data.frame(as.numeric(df[,5]),df[,7])
-colnames(df_test) <- c('value', 'type')
-ggplot(data = df_test, aes(x=value, y = ..density.., color = type, fill = type)) +
-  geom_histogram(alpha=0.5, position="identity") + theme_bw()
-
